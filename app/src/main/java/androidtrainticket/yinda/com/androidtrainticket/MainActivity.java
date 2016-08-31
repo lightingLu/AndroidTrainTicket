ee12096e88c029c481729465fee3521b741c384b
threelu <215154863@qq.com>
2016-08-31 10:18:36 +0800

    4
    登录界面的实现停留在此，接口错误。所以没法继续实现，接下老任务做查询界面

--- app/src/main/java/androidtrainticket/yinda/com/androidtrainticket/MainActivity.java
+++ app/src/main/java/androidtrainticket/yinda/com/androidtrainticket/MainActivity.java
@@ -1,13 +1,16 @@
 package androidtrainticket.yinda.com.androidtrainticket;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -18,16 +21,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.StreamBitmapDataLoadProvider;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import db.MyPoint;
 import http.HttpRequestUrl;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import utils.HTTPSUtils;
 
@@ -51,8 +59,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int startX;
     int startY;
     byte[] bytes;
-    String url = "https://kyfw.12306.cn/otn/passcodeNew/getPassCodeNew?module=login&rand=sjrand&0.012066099559888244";
-
+    StringBuilder sb=null ;
+    MyPoint points=null;
+    private static final MediaType MEDIA_TYPE_MARKDOWN
+            = MediaType.parse("text/x-markdown; charset=utf-8");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +74,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         utils = new HTTPSUtils(this);
         initViews();
         getPasswordCode2();
+        sb = new StringBuilder();
+        points = new MyPoint();
         loginLoginin.setOnClickListener(this);
         loginForget.setOnClickListener(this);
         loginLoginup.setOnClickListener(this);
@@ -77,7 +89,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     case MotionEvent.ACTION_DOWN:
                         startX = (int) motionEvent.getX();
                         startY = (int) motionEvent.getY();
-                        System.out.println(startX + startY + "------------------------------");
+                        System.out.println(startX + ":"+startY + "------------------------------");
+                         points.setX(startX);
+                         points.setY(startY) ;
+                         points.toString();
+                         sb.append(points.getX() + "," + points.getY());
+                         sb.append(",");
+                         System.out.println(sb.toString());
                         break;
                     //用户手指正在滑动
                     case MotionEvent.ACTION_MOVE:
@@ -90,9 +108,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         break;
                     //用户手指离开屏幕
                     case MotionEvent.ACTION_UP:
-
                         canvas.drawBitmap(bitmapYanzheng, startX, startY, paint);
-//                        canvas.drawCircle(startX,startY,25,paint);
                         loginPasscode.setImageBitmap(bitmapCopy);
                         break;
                 }
@@ -119,6 +135,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(getApplication(), "hello", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.login_loginin:
+                loginIn();
                 break;
             case R.id.login_loginup:
 //                Toast.makeText(getApplication(), "hello", Toast.LENGTH_SHORT).show();
@@ -133,12 +150,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /**
+     * 登录操作
+     */
+    private void loginIn() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String passCode = sb.toString();
+//                String path = HttpRequestUrl.PASSWOR_YANZ_URL+"randCode="+passCode+"&rand=sjrand";
+                OkHttpClient clientYanz = utils.getInstance(getApplication());
+                FormBody body = new FormBody.Builder()
+                        .add("randCode", passCode)
+                        .add("rand", "sjrand")
+                        .build();
+                Request requestYanz = new Request.Builder().url(HttpRequestUrl.PASSWOR_YANZ_URL).post(body).build();
+
+                try {
+                    Response response = clientYanz.newCall(requestYanz).execute();
+                    if (response.isSuccessful()){
+                        System.out.println("___________"+response);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+//        String password =loginPassword.getText().toString().trim();
+//        String username= loginName.getText().toString().trim();
+//        if (TextUtils.isEmpty(username)||TextUtils.isEmpty(password)){
+//            Toast.makeText(this,"用户名或密码不能为空！",Toast.LENGTH_LONG).show();
+//        }else {
+            Intent intent = new Intent(MainActivity.this,QueryActivity.class);
+            startActivity(intent);
+//        }
+    }
+
     public void getPasswordCode() {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 OkHttpClient client = utils.getInstance(getApplication());
-                Request request = new Request.Builder().url(url).build();
+                Request request = new Request.Builder().url(HttpRequestUrl.PASSWOR_CODE_URL).build();
                 try {
                     Response response = client.newCall(request).execute();
                     if (response.isSuccessful()) {
@@ -150,7 +205,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-//                                loginPasscode.setImageBitmap(bitmap);
                                 Glide.with(getApplication())
                                         .load(bytes)
                                         .into(loginPasscode);
@@ -175,7 +229,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void run() {
                 OkHttpClient client = utils.getInstance(getApplication());
-                Request request = new Request.Builder().url(url).build();
+                Request request = new Request.Builder().url(HttpRequestUrl.PASSWOR_CODE_URL).build();
+                 if (sb.length()!=0){
+                     sb.delete(0,sb.length());
+                 }
                 try {
                     Response response = client.newCall(request).execute();
                     if (response.isSuccessful()) {
--- app/src/main/java/http/HttpRequestUrl.java
+++ app/src/main/java/http/HttpRequestUrl.java
@@ -5,7 +5,19 @@ package http;
  */
 public class HttpRequestUrl {
     /**
-     * 图片验证码的链接
+     * 获取图片验证码的链接
      */
-    public static String PASSWOR_CODE_URL ="https://kyfw.12306.cn/otn/passcodeNew/getPassCodeNew?module=login&rand=sjrand&0.012066099559888244";
+    public  final static String PASSWOR_CODE_URL ="https://kyfw.12306.cn/otn/passcodeNew/getPassCodeNew?module=login&rand=sjrand&0.012066099559888244";
+    /**
+     * 图面验证码验证连接
+     */
+    public final static String PASSWOR_YANZ_URL ="https://kyfw.12306.cn/otn/passcodeNew/checkRandCodeAnsyn";
+    /**
+     * 用户登录连接
+     */
+    public  final  static String PASSWOR_LOGININ_URL ="http://kyfw.12306.cn/otn/login/loginAysnSuggest";
+    /**
+     * 用户登录状态连接
+     */
+    public  final static String PASSWOR_LOGIN_STATE_URL ="https://kyfw.12306.cn/otn/login/checkUser?json_att=;";
 }
